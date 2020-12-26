d7b1572300e7bf27e20c29c26393f5ff686b57e3
renqiankun <18819284248@163.com>
2020-12-27 00:51:28 +0800

    feat: autowire
    - 自动装配
      - byName根据属性名匹配（常用）
      - byType根据属性类型匹配

---
+++ src/test/org/bougainvillea/spring5/ioc/autowire/EmpTest.java
@@ -0,0 +1,32 @@
+package test.org.bougainvillea.spring5.ioc.autowire;
+
+import org.bougainvillea.spring5.ioc.autowire.Emp;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * 自动装配
+ * @author renqiankun
+ */
+public class EmpTest {
+
+    @Test
+    public void testAutowireByName(){
+        ApplicationContext context=new ClassPathXmlApplicationContext("source/ioc/autowire/autowireByName.xml");
+        Emp emp = context.getBean("emp", Emp.class);
+        System.out.println(emp);
+    }
+
+    /**
+     * 自动装配时 同一文件中不能定义相同类型的Bean
+     */
+    @Test
+    public void testAutowireByType(){
+        ApplicationContext context=new ClassPathXmlApplicationContext("source/ioc/autowire/autowireByType.xml");
+        Emp emp = context.getBean("emp", Emp.class);
+        System.out.println(emp);
+    }
+
+
+}
