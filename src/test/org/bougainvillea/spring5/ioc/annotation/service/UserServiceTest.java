100944c0b7467e0ae0b072cf41e8a52f4160556a
renqiankun <18819284248@163.com>
2020-12-28 00:04:21 +0800

    feat: 完全基于注解实现IOC

--- src/test/org/bougainvillea/spring5/ioc/annotation/service/UserServiceTest.java
+++ src/test/org/bougainvillea/spring5/ioc/annotation/service/UserServiceTest.java
@@ -1,8 +1,10 @@
 package test.org.bougainvillea.spring5.ioc.annotation.service;
 
+import org.bougainvillea.spring5.ioc.annotation.config.SpringConfig;
 import org.bougainvillea.spring5.ioc.annotation.service.UserService;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -10,9 +12,26 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class UserServiceTest {
 
+    /**
+     * 引入context名称空间，扫描注解
+     */
     @Test
     public void testAdd(){
         ApplicationContext context=new ClassPathXmlApplicationContext("source/ioc/annotation/annotation.xml");
+        UserService userService=context.getBean("userService",UserService.class);
+        userService.add();
+    }
+
+    /**
+     * 完全基于注解，扫描注解 等价于springboot
+     */
+    @Test
+    public void test(){
+        /**
+         * 加载配置类
+         */
+        ApplicationContext context=new AnnotationConfigApplicationContext(SpringConfig.class);
+
         UserService userService=context.getBean("userService",UserService.class);
         userService.add();
     }
