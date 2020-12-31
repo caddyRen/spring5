627e7c41fa9b295cabb9e2277acb721c3a44c414
renqiankun <18819284248@163.com>
2020-12-31 16:08:52 +0800

    feat: xml配置事务
    - 配置事务管理器
    - 配置advice
    - 配置切入点和切面

--- src/test/org/bougainvillea/spring5/jdbc/transaction/ServiceTest.java
+++ src/test/org/bougainvillea/spring5/jdbc/transaction/ServiceTest.java
@@ -1,6 +1,7 @@
 package test.org.bougainvillea.spring5.jdbc.transaction;
 
 import org.bougainvillea.spring5.jdbc.service.transaction.ServiceImpl;
+import org.bougainvillea.spring5.jdbc.service.transaction.xml.ServiceImpl2;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -15,6 +16,12 @@ public class ServiceTest {
     public void age(){
         ApplicationContext context=new ClassPathXmlApplicationContext("source/jdbc/datasource.xml");
         ServiceImpl serviceImpl = context.getBean("serviceImpl", ServiceImpl.class);
+        serviceImpl.age(1,2,1);
+    }
+    @Test
+    public void ageXml(){
+        ApplicationContext context=new ClassPathXmlApplicationContext("source/jdbc/xml/datasource.xml");
+        ServiceImpl2 serviceImpl = context.getBean("serviceImpl", ServiceImpl2.class);
         serviceImpl.age(1,2,1);
     }
 }
